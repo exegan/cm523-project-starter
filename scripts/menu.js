7f2b63f9e7a55180f5b3518d64e91f22b63548a7
exegan <exegan@bu.edu>
2023-04-23 12:51:41 -0400

    wip

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,5 +1,4 @@
-
-    let flashcardCount = 1;
+let flashcardCount = 1;
 
 function addFlashcard() {
   if (flashcardCount >= 20) {
@@ -30,21 +29,6 @@ function addFlashcard() {
 }
 
 
-fetch('./menu.json')
-.then((response) => response.json())
-.then((json) => console.log(json));
-.then(data => {
-    const menu= data.menu;
-    const newFront = prompt('Enter the Term');
-    const newBack = prompt('Enter the Definition');
-    const itemToUpdate = menu.find(item => item.id === itemId);
-
-    itemToUpdate.front = newFront;
-    itemToUpdate.back = newBack;
-
-    console.log(menu);
-}
-    )
 
 
 
