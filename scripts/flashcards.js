70dacd4662e429ff6368bc3f46422c37c4115113
exegan <exegan@bu.edu>
2023-04-25 10:07:49 -0400

    wip

--- scripts/flashcards.js
+++ scripts/flashcards.js
@@ -1,10 +1,4 @@
-const urlParams = new URLSearchParams(window.location.search);
-const flashcards = JSON.parse(decodeURIComponent(urlParams.get('flashcards')));
-
-const flashcardContainer = document.getElementById('flashcard-container');
-for (let i = 0; i < flashcards.length; i++) {
-  const flashcard = flashcards[i];
-  const newFieldset = document.createElement('fieldset');
-  newFieldset.innerHTML = `
-    <legend>Flashcard ${i + 1}</legend>
-    <div class="form-element">
+    fetch('menu.json')
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
