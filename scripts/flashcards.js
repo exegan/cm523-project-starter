b08e15d6d33c055b7d66faaad3b17f8756a20813
exegan <exegan@bu.edu>
2023-04-25 14:29:45 -0400

    wip

--- scripts/flashcards.js
+++ scripts/flashcards.js
@@ -1,4 +1,26 @@
     fetch('menu.json')
   .then(response => response.json())
-  .then(data => console.log(data))
+  .then(data => {
+    const flashcards = data.flashcards;
+    createFlashcards(flashcards);
+  })
   .catch(error => console.error(error));
+
+
+
+function createFlashcards(flashcards){
+
+    const container = document.getElementById('flashcard-container');
+    for (let i = 0; i < flashcards.length; i++) {
+        const card = document.createElement('div');
+        card.classList.add('flashcard');
+
+        card.innerHTML = `
+  <div class="front">${flashcards[i]['#term']}</div>
+  <div class="back">${flashcards[i]['#definition']}</div>
+`;
+
+container.appendChild(card);
+    }
+
+}
