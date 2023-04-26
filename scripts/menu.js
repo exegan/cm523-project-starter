b4b865061c04826f909bd3baaf58ef64d56fa357
exegan <exegan@bu.edu>
2023-04-25 20:07:01 -0400

    wip

--- scripts/menu.js
+++ scripts/menu.js
@@ -42,7 +42,7 @@ for (let i = 0; i < flashcardElements.length; i++) {
 
 localStorage.setItem('flashcardData', JSON.stringify(flashcardData));
 
-window.location.href = 'flashcards.html';
+/* window.location.href = 'flashcards.html'; */
 
 
 
