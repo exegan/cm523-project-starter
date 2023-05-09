9a8825263e3f000412939af7a8a9822a03972c02
exegan <exegan@bu.edu>
2023-05-08 20:16:52 -0400

    wip

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,58 +1,96 @@
-const flashcardForm = document.querySelector("#flashcard-form");
-const flashcardContainer = document.getElementById("flashcard-container");
-const questionTextarea = document.getElementById("question");
-const answerTextarea = document.getElementById("answer");
-const addFlashcardBtn = document.getElementById("addCardBtn");
-const submitBtn = document.getElementById("submitBtn");
+document.addEventListener("DOMContentLoaded", () => {
+  const studyTools = document.querySelectorAll(".study-tools");
+  const cardContainer = document.querySelectorAll(".card-container");
+  const flashcardCarousel = document.querySelector(".flashcard-carousel");
+  let flashcards = document.querySelectorAll(".flashcards .card");
+  const front = document.querySelectorAll(".front");
+  const back = document.querySelectorAll(".back");
+  const teacupContainer = document.querySelectorAll(".teacup-container");
+  const questionLabel = document.querySelectorAll(".question-label");
+  const answerLabel = document.querySelectorAll(".answer-label");
 
-addFlashcardBtn.addEventListener("click", function() {
-  const questionValue = questionTextarea.value;
-  const answerValue = answerTextarea.value;
-  const flashcardElement = document.createElement("div");
-  flashcardElement.classList.add("flashcard");
+  const question = document.getElementById("question");
+  const answer = document.getElementById("answer");
+  const nextBtn = document.getElementById("nextBtn");
+  const addFlashcard = document.getElementById("add-flashcard");
+  const backBtn = document.getElementById("backBtn");
+  const saveBtn = document.getElementById("saveBtn");
+  const flashcardsContainer = document.querySelector(".flashcards");
 
-  
-  addQuestion.addEventListener("click", () => {
-  container.classList.add("hide");
-  question.value = "";
-  answer.value = "";
-  addQuestionCard.classList.remove("hide");
-    
-    
-     const questionElement = document.createElement("div");
-  questionElement.classList.add("question");
-  questionElement.textContent = questionValue;
-  flashcardElement.appendChild(questionElement);
-  
-  const answerElement = document.createElement("div");
-  answerElement.classList.add("answer");
-  answerElement.textContent = answerValue;
-  flashcardElement.appendChild(answerElement);
- 
-  flashcardContainer.appendChild(flashcardElement);
-  
-  questionTextarea.value = "";
-  answerTextarea.value = "";
-});
+  let currentIndex = 0;
+  let totalCards = flashcards.length;
+  const cardWidth = flashcards[0].getBoundingClientRect().width;
+  const CARDS_PER_ROW = 1;
 
-submitBtn.addEventListener("click", function() {
+  // flip cards
+  function flipCard() {
+    if (this.classList.contains("flipped")) {
+      this.classList.remove("flipped");
+    } else {
+      this.classList.add("flipped");
+    }
+  }
 
-  const flashcardElements = flashcardContainer.querySelectorAll(".flashcard");
-  
-  flashcardElements.forEach(function(flashcardElement) {
-    const questionValue = flashcardElement.querySelector(".question").textContent;
-    const answerValue = flashcardElement.querySelector(".answer").textContent;
-    
-    console.log("Question:", questionValue);
-    console.log("Answer:", answerValue);
+  flashcards.forEach((card) => {
+    card.addEventListener("click", flipCard);
+  });
+
+  // add new card
+  addFlashcard.addEventListener("click", () => {
+    question.value = "";
+    answer.value = "";
+  });
+
+  // create and view card
+  function createCard(questionValue, answerValue) {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    const front = document.createElement("div");
+    front.classList.add("front");
+    front.textContent = questionValue;
+    const back = document.createElement("div");
+    back.classList.add("back");
+    back.textContent = answerValue;
+    card.appendChild(front);
+    card.appendChild(back);
+    flashcardsContainer.appendChild(card);
+
+    // update flashcards array
+    flashcards = document.querySelectorAll(".flashcards .card");
+
+    card.addEventListener("click", flipCard);
+  }
+
+  saveBtn.addEventListener("click", () => {
+    const questionValue = question.value;
+    const answerValue = answer.value;
+    createCard(questionValue, answerValue);
+    question.value = "";
+    answer.value = "";
+    totalCards = flashcards.length;
+  });
+
+  // carousel navigation
+  nextBtn.addEventListener("click", () => {
+    if (currentIndex < totalCards - 1) {
+      currentIndex++;
+      flashcardsContainer.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
+    }
+  });
+
+  backBtn.addEventListener("click", () => {
+    if (currentIndex > 0) {
+      currentIndex--;
+      flashcardsContainer.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
+    }
   });
 });
 
-document.addEventListener("click", function(event) {
- 
-  if (event.target.classList.contains("flip-card-btn")) {
-   
-    const flashcardElement = event.target.closest(".flashcard");
-   flashcardElement.classList.toggle("flipped");
-  }
-});
+
+
+
+ // Sources:
+//https://codingartistweb.com/2022/08/flashcard-app-with-javascript/
+//https://codingartistweb.com/2022/11/tea-cup-animation-css/
+//https://codepen.io/CraddockJ/pen/qBbQzXe
+//https://codepen.io/JPowell127/pen/BpOQWg
